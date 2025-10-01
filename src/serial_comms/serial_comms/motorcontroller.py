fcd4821530a0e0b07a93e7c8454585bdb7823da6
Tommy Le <tl3uk@virginia.edu>
2025-10-01 00:29:36 +0000

    Moved from training_node to motorcontroller and updated basic send_velocity

--- src/serial_comms/serial_comms/motorcontroller.py
+++ src/serial_comms/serial_comms/motorcontroller.py
@@ -2,9 +2,11 @@
 
 import rclpy #type: ignore
 from rclpy.node import Node #type: ignore
-from serial_msgs.msg import MotorCurrents
+from serial_msgs.msg import MotorCurrents, Feedback
 
-
+STOP_VAL = 127
+FORWARD_VAL = STOP_VAL + 27
+BACKWARD_VAL = STOP_VAL - 27
 
 class MotorControllerNode(Node):
     def __init__(self):
@@ -14,16 +16,30 @@ class MotorControllerNode(Node):
             topic='motor_currents',
             qos_profile = 1
         )
-        self.timer = self.create_timer(0.02, self.send_velocity)
+        # self.timer = self.create_timer(0.02, self.send_velocity)
+        self.feedback_subscriber_ =  self.create_subscription(
+            msg_type=Feedback,
+            topic="feedback",
+            callback=self.send_velocity,
+            qos_profile=10
+        )
 
-
-
-    def send_velocity(self):
+    def send_velocity(self, feedback):
         message = MotorCurrents()
-        message.left_wheels = 100 
-        message.right_wheels = 100
+        if feedback.front < 60:
+            if feedback.left < 30 and feedback.right > 30: # turn right
+                message.left_wheels = FORWARD_VAL
+                message.right_wheels = BACKWARD_VAL
+            elif feedback.left < 30 and feedback.right > 30: # turn left
+                message.left_wheels = BACKWARD_VAL
+                message.right_wheels = FORWARD_VAL
+            else: # stop
+                message.left_wheels = STOP_VAL
+                message.right_wheels = STOP_VAL
+        else:
+            message.left_wheels = FORWARD_VAL
+            message.right_wheels = FORWARD_VAL
         self.publisher.publish(message)
-
 
 def main(args=None):
     rclpy.init(args=args)
