b94211ac535310b4be788e9bb60c5497d09085df
aemmav <aemmav@icloud.com>
2025-10-03 22:54:20 +0000

    turn left test

--- src/publisher_node/publisher_node/publisher.py
+++ src/publisher_node/publisher_node/publisher.py
@@ -2,7 +2,7 @@ import rclpy
 from rclpy.node import Node, QoSProfile
 from rclpy.qos import QoSHistoryPolicy, QoSReliabilityPolicy, Duration
 from serial_msgs.msg import MotorCurrents
-
+from serial_msgs.msg import Feedback
 
 class SampleNode(Node):
 
@@ -10,14 +10,44 @@ class SampleNode(Node):
         super().__init__('test_node')
         self.publisher = self.create_publisher(
             msg_type=MotorCurrents,
-            topic='motor_currents', qos_profile=10)
-        self.timer = self.create_timer(0.5, self.send_velocity)
+            topic='motor_currents', 
+            qos_profile=10)
         
-    def send_velocity(self):
+        self.feedback_subscriber = self.create_subscription(
+            msg_type=Feedback,
+            topic='feedback',
+            qos_profile=1, 
+            callback=self.send_motor_currents)
+        s#elf.timer = self.create_timer(0.5, self.send_velocity)
+
+    def turn_left(self, feedback):
+
+        r_velo = 154
+        l_velo = 100
+        
+        return r_velo, l_velo
+        
+    def send_velocity(self, feedback):
         message = MotorCurrents()
-        message.left_wheels = 150
-        message.right_wheels = 150
+        r_velo = 150
+        l_velo = 150
+
+        # stops the robot if there is a wall 30cm ahead
+        # if feedback.us_sensor < 30:
+        #     r_velo = 127
+        #     l_velo = 127
+
+        # turns the robot left if there is a wall 30cm ahead
+        if feedback.us_sensor < 30:
+            r_velo, l_velo = self.turn_left(feedback)
+
+        message.right_wheels = r_velo
+        message.left_wheels = l_velo
+        
         self.publisher.publish(message)
+
+
+
 
 
     
