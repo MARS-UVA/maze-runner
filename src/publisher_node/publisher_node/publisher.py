f0a9e7d0e4dcfa7e4f437032946120de7080bff7
Emma Avetissian <167151011+aemmav@users.noreply.github.com>
2025-10-05 15:02:01 -0400

    Updated publisher.py with timer and turn left

--- src/publisher_node/publisher_node/publisher.py
+++ src/publisher_node/publisher_node/publisher.py
@@ -18,28 +18,44 @@ class SampleNode(Node):
             topic='feedback',
             qos_profile=1, 
             callback=self.send_velocity)
-        #self.timer = self.create_timer(0.5, self.send_velocity)
+        
+        self.timer = None
+        self.straight = True
+
+    #stop causes the timer to end
+    def stop(self):
+        self.straight = True
+        self.timer.cancel()
+        self.timer = None
 
     def turn_left(self, feedback):
+        
+        #if there is no timer currently active it starts the timer
+        if self.timer is None:
+            #timer calls stop after 5 seconds
+            self.timer = self.create_timer(5.0, self.stop)
 
+        self.straight = False
         r_velo = 230
-        l_velo = 24
+        l_velo = 70
         self.get_logger().info(f"jsadhfjhsdjafhjdahsfjhdjkfhdjshafjdkshfjkashdkfjhdsjkakf: glob")
         
         return r_velo, l_velo
         
     def send_velocity(self, feedback):
         message = MotorCurrents()
+
+        #initial velocities
         r_velo = 150
         l_velo = 150
 
         # stops the robot if there is a wall 30cm ahead
-        # if feedback.us_sensor < 30:
-        #     r_velo = 127
-        #     l_velo = 127
+        if feedback.front_sensor < 30:
+            r_velo = 127
+            l_velo = 127
 
-        # turns the robot left if there is a wall 30cm ahead
-        if feedback.us_sensor < 30:
+        # turns the robot left if there is no wall to the left within 30cm and it is currently going straight
+        if feedback.left_sensor > 30 and self.straight:
             r_velo, l_velo = self.turn_left(feedback)
 
         message.right_wheels = r_velo
