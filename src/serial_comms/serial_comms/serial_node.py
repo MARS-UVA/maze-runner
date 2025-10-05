cb97c856ba747aff4cdb12957f1cbddcf7957442
ZParsia <fyt2cd@virginia.edu>
2025-10-05 12:18:36 -0400

    Update serial_node.py for 3 sensor feedback

--- src/serial_comms/serial_comms/serial_node.py
+++ src/serial_comms/serial_comms/serial_node.py
@@ -9,7 +9,7 @@ from serial_msgs.msg import Feedback
 
 MOTOR_CURRENT_MSG = 0
 SEND_DELAY_SEC = 0.02
-RECV_DELAY_SEC = 0.03
+RECV_DELAY_SEC = 0.02
 MOTOR_STILL = 127
 
 class SerialNode(Node):
@@ -46,9 +46,9 @@ class SerialNode(Node):
     def readFromNucleo(self):
         data = self.serial_handler.readMsg(logger=self.get_logger())
         if data:
-            sensor_value = Feedback(us_sensor = data[0])
-            self.get_logger().warn(f"Ultrasonic data: {data[0]}")
-            self.feedback_publisher.publish(sensor_value)
+            sensor_values = Feedback(front_sensor = data[0], right_sensor = data[1], left_sensor = data[2])
+            self.get_logger().warn(f"Ultrasonic data: Front: {data[0]}, Right: {data[1]}, Left: {data[2]}")
+            self.feedback_publisher.publish(sensor_values)
         else:
             #self.feedback_publisher.publish(Feedback(us_sensor = 255)) # send 255 if no data
             self.get_logger().warn("no data")
