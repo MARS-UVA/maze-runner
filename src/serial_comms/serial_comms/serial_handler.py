35840f138e320e7b4d10a708cda22af675113bbf
ZParsia <fyt2cd@virginia.edu>
2025-10-05 12:19:30 -0400

    Update serial_handler.py for 3 sensor feedback

--- src/serial_comms/serial_comms/serial_handler.py
+++ src/serial_comms/serial_comms/serial_handler.py
@@ -33,15 +33,15 @@ class SerialHandler:
 		# self.SER.write(bytes([START]))
 		# self.SER.write(header.to_bytes(self.bytesPerMotor, byteorder="big"))
 		# logger.warn(f"Wrate {data}")
-		self.SER.write(bytes(data)) # write the data to serial port
+		self.SER.write(bytes([255]) + bytes(data)) # write the data to serial port
 		logger.info(f"Wrote {data} to microcontroller")
 
 	def readMsg(self, logger=None):
 		logger.info(f'Serial bytes in waiting: {self.SER.in_waiting}')
-		if(self.SER.in_waiting<2): return []
-		elif(self.SER.in_waiting>4): self.SER.read((self.SER.in_waiting//2)*2)
+		if(self.SER.in_waiting<4): return []
+		elif(self.SER.in_waiting>4): self.SER.read((self.SER.in_waiting-4))
 		header = self.SER.read(1)
-		feedback = list(struct.iter_unpack("B",self.SER.read(1))) # tuple of: ir_sensor
+		feedback = list(struct.iter_unpack("B",self.SER.read(3))) # tuple of: us_sensor
 		feedback = [i[0] for i in feedback]
 		return feedback
 
