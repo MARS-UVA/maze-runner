3d2e152e9a666b4afb15e16bedda52ded9fb4b48
aemmav <aemmav@icloud.com>
2025-10-17 23:05:45 +0000

    fix

--- src/publisher_node/publisher_node/publisher.py
+++ src/publisher_node/publisher_node/publisher.py
@@ -22,7 +22,7 @@ class SampleNode(Node):
             callback=self.send_velocity)
         
 
-    def turn_left(self, feedback):
+    def turn_left(self):
 
         # turn velocities
         r_velo, l_velo = 230, 70
@@ -54,7 +54,7 @@ class SampleNode(Node):
 
         # turns left if there is a wall 30cm ahead
         if feedback.front_sensor < 30:
-            self.turn_left(feedback)
+            r_velo, l_velo = self.turn_left()
 
 
         message.right_wheels = r_velo
