99f98440325bd2a13e2184c18299be3b532637b4
Tommy Le <tl3uk@virginia.edu>
2025-10-19 17:41:31 +0000

    Removed timer

--- src/serial_comms/serial_comms/motorcontroller.py
+++ src/serial_comms/serial_comms/motorcontroller.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-
+import time
 import rclpy #type: ignore
 from rclpy.node import Node #type: ignore
 from serial_msgs.msg import MotorCurrents, Feedback
@@ -11,8 +11,6 @@ LEFT_DIST = 30
 RIGHT_DIST = 30
 FRONT_DIST = 15
 TURN_TIME = 3.35
-FORWARD_TIME = 0.65
-S_TO_NS = 1000000000
 
 class MotorControllerNode(Node):
     def __init__(self):
@@ -29,47 +27,36 @@ class MotorControllerNode(Node):
             callback=self.send_velocity,
             qos_profile=10
         )
-        self.turn_timer = self.create_timer(3.35, self.stop)
-        self.is_turning = False
 
     def send_velocity(self, feedback):
         message = MotorCurrents()
-        if feedback.left_sensor > LEFT_DIST and not self.is_turning: # left opening, turn left
-            self.turn(message, "left", TURN_TIME + FORWARD_TIME) # time to turn and move forward a bit
-        elif feedback.front_sensor > FRONT_DIST and not self.is_turning: # go forward
+        if feedback.left_sensor > LEFT_DIST: # left opening, turn left
+            self.turn(message, "left", TURN_TIME)
+        elif feedback.front_sensor > FRONT_DIST: # go forward
             message.left_wheels = FORWARD_VAL
             message.right_wheels = FORWARD_VAL
-        elif feedback.right_sensor > RIGHT_DIST and not self.is_turning: # right opening, turn right
-            self.turn(message, "right", TURN_TIME + FORWARD_TIME) # time to turn and move forward a bit
-        elif not self.is_turning: # turn around
-            self.turn(message, "left", TURN_TIME) # no forward time, so should only perform turn
-            self.turn(message, "left", TURN_TIME) # no forward time, so should only perform turn
-        else:
-            return
-        self.publisher.publish(message)
+            self.publisher.publish(message)
+        elif feedback.right_sensor > RIGHT_DIST: # right opening, turn right
+            self.turn(message, "right", TURN_TIME)
+        else: # turn around
+            self.turn(message, "left", TURN_TIME)
+            self.turn(message, "left", TURN_TIME)
+        if feedback.front_sensor > FRONT_DIST: # go forward
+            message.left_wheels = FORWARD_VAL
+            message.right_wheels = FORWARD_VAL
+            self.publisher.publish(message)
+            time.sleep(0.5)
 
-    def turn(self, message, dir, time):   
+    def turn(self, message, dir, duration):   
         if dir == "right":
-            if self.turn_timer.time_since_last_call() <= TURN_TIME * S_TO_NS: # still in turn
-                message.left_wheels = FORWARD_VAL
-                message.right_wheels = BACKWARD_VAL
-            else: # go forward a bit after turn
-                message.left_wheels = FORWARD_VAL
-                message.right_wheels = FORWARD_VAL
+            message.left_wheels = FORWARD_VAL
+            message.right_wheels = FORWARD_VAL
         else: # assume left
-            if self.turn_timer.time_since_last_call() <= TURN_TIME * S_TO_NS: # still in turn
-                message.left_wheels = BACKWARD_VAL
-                message.right_wheels = FORWARD_VAL
-            else: # go forward a bit after turn
-                message.left_wheels = FORWARD_VAL
-                message.right_wheels = FORWARD_VAL
+            message.left_wheels = FORWARD_VAL
+            message.right_wheels = FORWARD_VAL
         self.get_logger().info("Turning")
-        self.publisher.publish(message)  
-        self.is_turning = True
-        if (self.turn_timer.is_canceled):
-            self.turn_timer.reset()
-        else:
-            self.turn_timer = self.create_timer(time, self.stop)
+        self.publisher.publish(message)
+        time.sleep(duration)
             
 
     def stop(self):
@@ -77,8 +64,6 @@ class MotorControllerNode(Node):
         message.left_wheels = STOP_VAL
         message.right_wheels = STOP_VAL
         self.publisher.publish(message)
-        self.turn_timer.cancel()
-        self.is_turning = False
 
 def main(args=None):
     rclpy.init(args=args)
